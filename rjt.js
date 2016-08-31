3d9c8807f6ed6b1d923f2feb83628a4ac204e6ba
Ian Witherow <ian.witherow@sourceot.com>
2016-08-31 11:47:38 -0600

    Make sure content is not null - set to empty string if it is

--- rjt.js
+++ rjt.js
@@ -192,26 +192,29 @@ var Row = React.createClass({
 			cellClass = this.getSetting('cellClass'),
 			rowClass = this.getSetting('rowClass'),
 			prefix = this.getSetting('classPrefix'),
-			cells = props.columns.map( function( col ){
+			cells = props.columns.map(function(col) {
 				var content = col.cell,
-					key = col.key,
-					className = prefix + 'Cell ' + prefix + 'Cell_' + key
+				key = col.key,
+				className = prefix + 'Cell ' + prefix + 'Cell_' + key
 				;
 
 				if( cellClass )
-					className = cellClass( className, key, props.item );
+				className = cellClass( className, key, props.item );
 
-				if( typeof content == 'function' )
-					content = content( props.item, key );
+				if( typeof content == 'function' ) {
+					content = content(props.item, key);
+				}
+				// Make sure content is not null
+				content = content || '';
 
-					if (content.indexOf('$') === 0) {
-						// Currency - align right
-						className += " text-right";
-					}
-					if (content.trim().length === 0) {
-						// Put in a space so the row isn't all stupid
-						content = '<div>&nbsp;</div>';
-					}
+				if (content.toString().indexOf('$') === 0) {
+					// Currency - align right
+					className += " text-right";
+				}
+				if (content.toString().trim().length === 0) {
+					// Put in a space so the row isn't all stupid
+					content = '<div>&nbsp;</div>';
+				}
 
 
 				return $.td( {
